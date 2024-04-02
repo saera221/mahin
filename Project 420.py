b0ecabe513392bb89df9c2e381beb7851e10c65c
saera221 <85393626+saera221@users.noreply.github.com>
2024-04-02 14:01:04 +0600

    Add files via upload

---
+++ Project 420.py
@@ -0,0 +1,47 @@
+# My Name
+#data type variable e '' must
+print('Your phone is under my gain jutsu😵😵')
+print('1....>')
+print('2.......>')
+print('3...........>')
+print("Hacking started...")
+print('Hacking 00%')
+print('Hacking 05%')
+print('Hacking 10%')
+print('Hacking 20%')
+print('Hacking 25%')
+print('Hacking 35%')
+print('Hacking 50%')
+print('Hacking 75%')
+print('Hacking 80%')
+print('Hacking 100%')
+print("=>")
+print("===>")
+print("======>")
+print("============>")
+print("===================>")
+print("==========================>")
+print("=================================>")
+print("======================================>")
+print("=========================================>")
+print("==========================================>")
+print("==========================================>")
+print('Completed')
+
+print(input("Hey buddy! Who are you? Plese type your Name to conform:"))
+
+print("Let's intruduce the great nasa hacker!!!😁😁😁😁")
+
+sortname = 'Kazi Mahin'
+Roll_no = 731788
+print('Kazi Mahinuzzaman')
+print(sortname,f'Roll:{Roll_no}')
+
+
+#user input
+Parents = input("Father's name:")
+print(Parents)
+parents = input("Mother's name:")
+print(parents)
+
+
